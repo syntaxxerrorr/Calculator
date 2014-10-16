4bdd61042f30039fcc56f60d2919225501e9f2c7
syntaxxerrorr <fahad@Fahads-MacBook-Pro.local>
2014-10-16 17:21:32 +0500

    Start

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -8,6 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
+
+int Method;
+int SelectNumber;
+float RunningTotal;
+
 @interface ViewController : UIViewController
+{
+    IBOutlet UILabel *Screen;
+    
+}
+-(IBAction)Number1:(id)sender;
+-(IBAction)Number2:(id)sender;
+-(IBAction)Number3:(id)sender;
+-(IBAction)Number4:(id)sender;
+-(IBAction)Number5:(id)sender;
+-(IBAction)Number6:(id)sender;
+-(IBAction)Number7:(id)sender;
+-(IBAction)Number8:(id)sender;
+-(IBAction)Number9:(id)sender;
+-(IBAction)Number0:(id)sender;
+-(IBAction)Times:(id)sender;
+-(IBAction)Devide:(id)sender;
+-(IBAction)Subtract:(id)sender;
+-(IBAction)Plus:(id)sender;
+-(IBAction)Equals:(id)sender;
+-(IBAction)AllClear:(id)sender;
+
 
 @end
